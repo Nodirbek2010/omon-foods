130cb291bbb41f4daf8fe1e72f335f6166cd03d2
xasano5o <xasansenior@gmail.com>
2024-02-07 19:33:51 +0500

    sidebar

---
+++ src/components/admin/Layout/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {  Outlet } from "react-router-dom";
+import Sidebar from "../sidebar";
+
+export default function Layout() {
+  const token = "1";
+
+  return (
+        <div className="flex">
+          < Sidebar/>
+          <div className="w-full layout">
+            <Outlet />
+          </div>
+        </div>
+  );
+}
