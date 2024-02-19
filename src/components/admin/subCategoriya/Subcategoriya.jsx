1a029780f2ed15b532f56ad006c66ba11bb5da92
Nodirbekakm2205 <nodirbekakm2205@gmail.com>
2024-02-19 18:37:17 +0500

    hello

--- src/components/admin/subCategoriya/Subcategoriya.jsx
+++ src/components/admin/subCategoriya/Subcategoriya.jsx
@@ -49,8 +49,8 @@ const Products = () => {
                             return (
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        <img src="" alt="" />
-                                        {value?.thumbnail_image}
+                                        <img src={value?.thumbnail_image} alt="" />
+                                        
                                     </th>
                                     <td class="px-6 py-4">
                                         {value?.category?.title
