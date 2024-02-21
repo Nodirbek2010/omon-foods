9f2ab4cfed0ecafc6997fcadef17f4859d160ad2
xasano5o <xasansenior@gmail.com>
2024-02-21 17:13:37 +0500

    Add

--- src/components/admin/Products/products.jsx
+++ src/components/admin/Products/products.jsx
@@ -50,8 +50,8 @@ const Products = () => {
 
                     return(
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                        <img src="" alt="" />
+                        <th className="flex" scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                        <img className="w-[50px] h-[50px] rounded-lg " src={value?.image} alt="" />
                            {value?.title}
                         </th>
                         <td class="px-6 py-4">
