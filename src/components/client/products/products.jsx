9642a4184e7b6d4376f8959e630f9dbee73167a6
senior2009 <sayfiddinsadikov8@gmail.com>
2024-03-13 17:44:43 +0500

    xl

--- src/components/client/products/products.jsx
+++ src/components/client/products/products.jsx
@@ -19,12 +19,13 @@ const ProductCom = () => {
     return (
         <div className='grid grid-cols-4 gap-5 m-auto justify-center'>
             {data?.map((value, index) => (
-                <div key={index}>
+                <div className=' relative' key={index}>
                     <CiHeart
                         onClick={() => toggleLike(value.id)}
-                        className={` ${likedProducts.includes(value.id) ? 'bg-red-500 rounded-3xl' : ""} w-[40px] h-[30px] mt-3 ml-5 `}
+                        className={` ${likedProducts.includes(value.id) ? 'bg-red-500 rounded-3xl' : ""} absolute left-64 top-3 w-[40px] h-[30px] mt-3 ml-5 `}
                     />
-                    <img className='cursor-copy object-contain w-[270px] h-[250px] p-3' src={value.image} alt="" />
+                    <img className='cursor-copy object-contain w-full p-3 h-[450px] ' src={value.image} alt="" />
+                    <button className=' hover:bg-black  absolute top-96 bg-white text-white w-[320px] h-[56px] ml-3'>Add to card</button>
                     <h1>{value?.title}</h1>
                     {value?.price} so'm
                 </div>
