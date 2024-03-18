3a6dd1af74fa877eba70c5a5aeadb83476551f4e
senior2009 <sayfiddinsadikov8@gmail.com>
2024-03-18 17:53:35 +0500

    byd song

--- src/components/client/NewArrival/arrival.jsx
+++ src/components/client/NewArrival/arrival.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import Features from '../../../assets/image/Frame 619.png'
-import img_3 from '../../../assets/image/Frame 738.png'
+import PS3 from '../../../assets/image/Frame 684 (1).png'
+import Cap from '../../../assets/image/Frame 685.png'
+import Speakers from '../../../assets/image/Frame 686.png'
+import Pertfume from '../../../assets/image/Frame 687.png'
+
 
 const ArrivalCom = () => {
   return (
     <div>
-        <img src={Features} alt="" />
-        <div className='text-3xl mt-4'>
-            <span>New Arrival</span>
-        </div>
-        <div>
-            <img className='w-[570px] h-[600px]' src={img_3} alt="" />
-        </div>
+      <img src={Features} alt="" />
+      <div className='text-3xl mt-4'>
+        <span>New Arrival</span>
+      </div>
+      <div className='img_com'>
+          <div>
+             <img src={PS3} alt="" /> 
+          </div>
+          <div className='gap'>
+              <div>
+              <img src={Cap} alt="" />
+              </div>
+              <div className='image_2'>
+                <img src={Speakers} alt="" />
+                <img src={Pertfume} alt="" />
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
