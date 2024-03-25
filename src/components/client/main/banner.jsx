18951baaf8ca79c8d40132324fbf1b83657f72ec
xasano5o <xasansenior@gmail.com>
2024-03-25 17:53:01 +0500

    Banner >:)

--- src/components/client/main/banner.jsx
+++ src/components/client/main/banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Carousel } from 'antd';
+import { useGetBannersQuery } from "../../../redux/Slice/banner/slice"
+
 const contentStyle = {
   height: '160px',
   color: '#fff',
@@ -7,21 +9,24 @@ const contentStyle = {
   textAlign: 'center',
   background: '#364d79',
 };
-const Banner1 = () => (
-  <Carousel >
-    <div>
-      <h3 style={contentStyle}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>
-  </Carousel>
 
-);
-export default Banner1;
+const App = () => {
+  const { data: BannersData, isLoading } = useGetBannersQuery();
+
+  
+
+  return (
+    <Carousel autoplay>
+      {BannersData?.map((value) => {
+        console.log(value,'value');
+      return(
+        <div className='bannrimg'>
+        <img className=" bannerimg w-full" src={value?.image} alt="" />
+      </div>
+      )     
+})}
+    </Carousel>
+  );
+};
+
+export default App;
