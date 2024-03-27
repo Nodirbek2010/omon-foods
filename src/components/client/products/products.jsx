86fcc73d2c88be05c398547e3d4e356151f23e14
senior2009 <sayfiddinsadikov8@gmail.com>
2024-03-27 17:06:31 +0500

    zx

--- src/components/client/products/products.jsx
+++ src/components/client/products/products.jsx
@@ -19,7 +19,7 @@ const ProductCom = () => {
     // 
 
     const addData = async (id) => {
-        console.log(id,'id');
+        console.log(id, 'id');
         const formData = new FormData();
         formData.append("amount", 1);
         formData.append("product", id);
@@ -32,19 +32,22 @@ const ProductCom = () => {
     };
 
     return (
-        <div className='grid grid-cols-4 gap-5 m-auto justify-center'>
-            {data?.map((value, index) => (
-                <div className=' relative' key={index}>
-                    <CiHeart
-                        onClick={() => toggleLike(value.id)}
-                        className={` ${likedProducts.includes(value.id) ? 'bg-red-500 rounded-3xl' : ""} absolute left-64 top-3 w-[40px] h-[30px] mt-3 ml-5 `}
-                    />
-                    <img className='cursor-copy object-contain w-full p-3 h-[450px] ' src={value.image} alt="" />
-                    <button onClick={() => addData(value.id)} className=' hover:bg-black  absolute top-96 bg-white text-white w-[320px] h-[56px] ml-3'>Add to card</button>
-                    <h1>{value?.title}</h1>
-                    {value?.price} so'm
-                </div>
-            ))}
+        <div>
+            <h1 className='text-3xl pl-2 pt-8'>Products</h1>
+            <div className='grid grid-cols-4 gap-5 m-auto justify-center'>
+                {data?.map((value, index) => (
+                    <div className=' relative' key={index}>
+                        <CiHeart
+                            onClick={() => toggleLike(value.id)}
+                            className={` ${likedProducts.includes(value.id) ? 'bg-red-500 rounded-3xl' : ""} absolute left-64 top-3 w-[40px] h-[30px] mt-3 ml-5 `}
+                        />
+                        <img className='cursor-copy object-contain w-full p-3 h-[450px] ' src={value.image} alt="" />
+                        <button onClick={() => addData(value.id)} className=' hover:bg-black  absolute top-96 bg-white text-white w-[320px] h-[56px] ml-3'>Add to card</button>
+                        <h1>{value?.title}</h1>
+                        {value?.price} so'm
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
