a9e2b1bb4e0fad2a3db4a138e2656c38e51e5d44
senior2009 <sayfiddinsadikov8@gmail.com>
2024-03-29 17:29:56 +0500

    xz

--- src/components/client/Sign_up/sign_up.jsx
+++ src/components/client/Sign_up/sign_up.jsx
@@ -7,13 +7,18 @@ const Sign_upCom = () => {
         <div>
           <img src={Side_img} alt="" />
         </div>
-        <div>
+        <div className='flex  flex-col justify-center items-center gap-10'>
           <h1 className=' font-semibold text-5xl'>Create an account</h1>
-          <div>
+          <div className=' text-2xl font-normal '>
             <p>Enter your details below</p>
           </div>
-          <div>
-            
+          <div className=''>
+            <input className='w-[370px] h-[32px] border-none ' type="text"  placeholder='Name'/> <br/>
+            <hr  className=' w-[370px]'/>
+            <input className='w-[370px] h-[32px] border-none ' type="email"  placeholder='Email or Phone number'/><br/>
+            <hr  className='w-[370px]'/>
+            <input className='w-[370px] h-[32px] border-none ' type="password" name="" id="" placeholder='Password' />
+            <hr className='w-[370px]' />
           </div>
         </div>
     </div>
